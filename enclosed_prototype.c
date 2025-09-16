09ab4a367048290c60ef8d73bb33d14d180cb2be
Ian Snider <iqsnider@gmail.com>
2025-09-15 23:02:05 -0700

    feat: 2D memory allocation management

--- enclosed_prototype.c
+++ enclosed_prototype.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
+// iterative solver and 2D array memory allocator
+double** solveShape(int n, int m, double arr[n][m],
+		    double new_internal_angle, double tolerance){
+	double** result_matrix = malloc(n*sizeof(double));
+	for(int i = 0; i < n; i++){
+		result_matrix[i] = malloc(m*sizeof(double));
+		memcpy(result_matrix[i], arr[i], m*sizeof(double));
+	}
 
-// double** solveShape(int n, int m, int arr[n][m]){
-//
-// }
+	return result_matrix;
+}
 
+// print all vertice coordinates
 void print2Darray(int n, int m, double arr[n][m]) {
 	for (int i = 0; i < n; i++){
 		for (int j = 0; j < m; j++){
@@ -15,6 +25,7 @@ void print2Darray(int n, int m, double arr[n][m]) {
 	}
 }
 
+// Yay!
 int main() {
 	int total_nodes = 4;
 
@@ -25,8 +36,9 @@ int main() {
 	shape[1][0] = 1.0;
 	shape[1][1] = 0.0;
 	shape[2][0] = 0.5;
-	shape[2][1] = 1.0;
+	shape[2][1] = sqrt(shape[2][0]*shape[2][0] + 1);
 
+	solveShape(total_nodes, 2, shape, M_PI, 0.0000001);
 	print2Darray(total_nodes, 2, shape);
 
 	// for (int i=0;i<total_nodes;i++){
@@ -34,4 +46,6 @@ int main() {
 	// 	double random_modifier = random_number*1.0f/(total_nodes*1.0f);
 	//
 	// }
+
+	return 0;
 }
