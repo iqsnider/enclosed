35cb2a5fdf0e82b5638198526b5a9df6570d4267
Ian Snider <iqsnider@gmail.com>
2025-09-17 01:08:44 -0700

    feat: added segment verification

--- enclosed_prototype.c
+++ enclosed_prototype.c
@@ -3,36 +3,29 @@
 #include <math.h>
 #include <string.h>
 
-// iterative solving and 2D array memory allocator
+// iterative solving
 double** solveShape(int n, int m, double arr[n][m],
 		    double new_internal_angle, double step_size){
 	double** result_matrix = malloc(n*sizeof(double));
 
-	int i = 2;
-	// special logic solving case
-
 	// general location of added node
-	arr[i][0] = arr[i-1][0] + cos(new_internal_angle);
-	arr[i][1] = arr[i-1][1] + sin(new_internal_angle);
+	arr[0][0] = arr[n-1][0] + cos(new_internal_angle);
+	arr[0][1] = arr[n-1][1] + sin(new_internal_angle);
 
 	// solve for the new location of the node next to added node
 	// such that all segments remain a length of 1
 	for (double j = 0; j <= M_PI; j += step_size){
-		double x = arr[i][0] + cos(new_internal_angle + j);
-		double y = arr[i][1] + sin(new_internal_angle + j);
-		// printf("(%.5f, %.5f)", x, y);
+		double x = arr[0][0] + cos(new_internal_angle + j);
+		double y = arr[0][1] + sin(new_internal_angle + j);
 
-		// double length_to_next_node = sqrt((x - arr[i+1][0])*(x - arr[i+1][0]) + (y - arr[i+1][1])*(y - arr[i+1][1]));
-		double length_to_next_node = sqrt(x*x + y*y);
-		// printf("%.5f\n", length_to_next_node);
+		// check the segment length
+		double length_to_next_node = sqrt((x - arr[1][0])*(x - arr[1][0]) + (y - arr[1][1])*(y - arr[1][1]));
 		if (length_to_next_node <= 1 + step_size && length_to_next_node >= 1 - step_size){
-			arr[i+1][0] = x;
-			arr[i+1][1] = y;
+			arr[1][0] = x;
+			arr[1][1] = y;
 			break;
 		}
 	}
-
-
 	
 	for(int i = 0; i < n; i++){
 		result_matrix[i] = malloc(m*sizeof(double*));
@@ -58,12 +51,12 @@ int main() {
 
 	// initialize with triangle
 	double shape[total_nodes][2];
-	shape[0][0] = 0.0;
-	shape[0][1] = 0.0;
-	shape[1][0] = 1.0;
-	shape[1][1] = 0.0;
-	shape[total_nodes-1][0] = 0.5;
-	shape[total_nodes-1][1] = sqrt(1 - shape[total_nodes-1][0]*shape[total_nodes-1][0]);
+	shape[total_nodes-1][0] = 0.0;
+	shape[total_nodes-1][1] = 0.0;
+	shape[total_nodes-2][0] = 1.0;
+	shape[total_nodes-2][1] = 0.0;
+	shape[total_nodes-3][0] = 0.5;
+	shape[total_nodes-3][1] = sqrt(1 - shape[total_nodes-1][0]*shape[total_nodes-1][0]);
 
 	double** result = solveShape(total_nodes, 2, shape, M_PI/4, 0.1);
 	print2Darray(total_nodes, 2, result);
